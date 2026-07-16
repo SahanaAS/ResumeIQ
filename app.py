f065c6650ab3cdf41168192a3e0d29292e5ae5c0
Sahana AS <sahana.a.swamy@gmail.com>
2026-07-15 22:16:00 -0400

    Integrated Claude API into ResumeIQ

--- app.py
+++ app.py
@@ -1,44 +1,60 @@
 import streamlit as st
+import os
+from dotenv import load_dotenv
+import anthropic
 
-st.title("ResumeIQ")
-st.subheader("AI Resume Reviewer")
+# Load API key
+load_dotenv()
 
-resume = st.text_area(
-    "Paste your Resume",
-    height=250
+client = anthropic.Anthropic(
+    api_key=os.getenv("ANTHROPIC_API_KEY")
 )
 
-job = st.text_area(
-    "Paste the Job Description",
-    height=250
-)
+st.title("🤖 AI Resume Reviewer")
 
-if st.button("🚀 Analyze Resume"):
+resume = st.text_area("Paste your Resume", height=250)
+
+job = st.text_area("Paste the Job Description", height=250)
+
+if st.button("Analyze Resume"):
 
     if resume.strip() == "" or job.strip() == "":
-        st.warning("⚠️ Please paste both your resume and the job description.")
-
+        st.warning("Please enter both the Resume and Job Description.")
     else:
 
-        st.success("Resume received successfully!")
+        with st.spinner("Claude is analyzing your resume..."):
 
-        st.divider()
+            prompt = f"""
+You are an expert technical recruiter.
 
-        st.header("📊 Analysis Results")
+Analyze this resume against the job description.
 
-        st.metric(
-            label="Resume Match Score",
-            value="Coming Soon..."
-        )
+Resume:
+{resume}
 
-        st.subheader("💪 Strengths")
-        st.info("Claude will identify your strengths here.")
+Job Description:
+{job}
 
-        st.subheader("📚 Missing Skills")
-        st.info("Claude will list missing skills here.")
+Provide:
 
-        st.subheader("📈 Suggested Improvements")
-        st.info("Claude will suggest improvements here.")
+1. Match Score (0-100)
+2. Top Strengths
+3. Missing Skills
+4. Resume Improvements
+5. Final Verdict
+"""
 
-        st.subheader("🎤 Interview Questions")
-        st.info("Claude will generate interview questions here.")
+            response = client.messages.create(
+              model="claude-haiku-4-5-20251001",
+                max_tokens=2000,
+                messages=[
+                    {
+                        "role": "user",
+                        "content": prompt
+                    }
+                ]
+            )
+
+            st.success("Analysis Complete!")
+
+            st.write(response.content[0].text)
